64f19f32bbb819410e47b294dceb389365d3552e
Emma.nu <tokyoep@gmail.com>
2023-05-06 01:34:32 -0400

    gather img data from the api

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [sourcePokemon, setSourcePokemon] = useState(null)
+  const [click, setClick] = useState(true)
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(data => data.json())
+      .then(json => {
+        console.log(json.sprites.other["official-artwork"].front_default);
+        setSourcePokemon(json.sprites.other["official-artwork"].front_default);
+      })
+
+  }, [click]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Guess the Pokemon !</Text>
+      <Image style={styles.img} source={{
+        uri: sourcePokemon
+      }} />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -16,5 +32,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  text: {
+    fontSize: 30,
+    margin: 10
+  },
+  img: {
+    width: 200,
+    height: 200,
+    aspectRatio: 3 / 2,
+    resizeMode: "contain",
+    margin: 10
+  }
 });
